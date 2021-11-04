f1d4dfdf436427ebe97d5904c3e68649f6a2b54e
Marie Beaujeu <marie.beaujeu@gmail.com>
2021-11-04 10:18:08 +0100

    Heorku update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const messageCtrl = require('./controllers/messageCtrl');
 
 const app = express();
 
-const whitelist = ['http://127.0.0.1', 'http://127.0.0.1:3000', 'http://localhost:3000'];
+const whitelist = ['http://127.0.0.1', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://192.168.1.19:3000', 'https://portfolio-mb1.herokuapp.com'];
 
 mongoose.connect(process.env.DB_MONGO_URL)
   .then(() => console.log('Connected to MongoDB'))
@@ -17,6 +17,7 @@ mongoose.connect(process.env.DB_MONGO_URL)
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
+// app.use(cors());
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (whitelist.indexOf(origin) > -1) {
